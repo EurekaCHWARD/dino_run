b52254b077a69089b02184156669988dbca29fd5
EurekaCHWARD <eurekadiaandisy8@gmail.com>
2024-01-02 00:15:19 +0700

    idk

--- lib/auth/services/auth_service.dart
+++ lib/auth/services/auth_service.dart
@@ -2,23 +2,38 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthService {
-  //Google sign in
-  signInWithGoogle() async {
-    //begin interactive sign in process
-    final GoogleSignInAccount? gUser = await GoogleSignIn(
-      clientId: '988810755372-3ff8q17q20idhg0vfjh935tp12vtur27.apps.googleusercontent.com')
-      .signIn();
+  // Google sign in
+  Future<User?> signInWithGoogle() async {
+    try {
+      // Begin interactive sign-in process
+      final GoogleSignInAccount? gUser = await GoogleSignIn(
+        clientId: '988810755372-n3dleol1d7r8lf6u612f7b328mepm7hc.apps.googleusercontent.com',
+        serverClientId:'988810755372-qigqgof1d2cfo6agc6894bg224tt6nd5.apps.googleusercontent.com',
+        scopes: ['email'],
+      ).signIn();
 
-    //obtain auth details from request
-    final GoogleSignInAuthentication gAuth = await gUser!.authentication;
+      // Handle cancellation or null user
+      if (gUser == null) {
+        print("Google Sign-In canceled");
+        return null;
+      }
 
-    //create new credentials for user
-    final credential = GoogleAuthProvider.credential(
-      accessToken: gAuth.accessToken,
-      idToken: gAuth.idToken,
-    );
+      // Obtain auth details from request
+      final GoogleSignInAuthentication gAuth = await gUser.authentication;
 
-    //sign in!
-    return await FirebaseAuth.instance.signInWithCredential(credential);
+      // Create new credentials for user
+      final credential = GoogleAuthProvider.credential(
+        accessToken: gAuth.accessToken,
+        idToken: gAuth.idToken,
+      );
+
+      // Sign in
+      final UserCredential userCredential =
+          await FirebaseAuth.instance.signInWithCredential(credential);
+      return userCredential.user;
+    } catch (e) {
+      print("Error signing in with Google: $e");
+      return null;
+    }
   }
 }
